71631d6ebab727ef54bf48753a183d6774db7f86
Jan Lelis <mail@janlelis.de>
2011-01-05 18:03:47 +0100

    refactored code + support for ranges (fixes gh#1)

--- lib/unicode/display_width.rb
+++ lib/unicode/display_width.rb
@@ -1,68 +1,62 @@
 # encoding: utf-8
 module Unicode; end
+module Unicode::DisplayWidth; end
 
-module Unicode::DisplayWidth
-  VERSION = '0.1.0'
+class << Unicode::DisplayWidth
+  VERSION = '0.1.1'
 
   DATA_DIR = File.join(File.dirname(__FILE__), '../../data/')
-  FIELDS = [:codepoint, :width, :name]
-  INDEX_FILE = DATA_DIR + 'EastAsianWidth.index'
+  TABLE_FILE = DATA_DIR + 'EastAsianWidth.index'
   DATA_FILE  = DATA_DIR + 'EastAsianWidth.txt'
 
-  def self.data
-    @@data ||= File.open DATA_FILE
+  # only needed for building the index
+  def data
+    @data ||= File.open DATA_FILE
   end
 
-  def self.offsets
-    @@offsets ||= Marshal.load File.respond_to?(:binread) ? File.binread(INDEX_FILE) : File.read(INDEX_FILE)
-  end
-
-  class Codepoint < Struct.new(*FIELDS)
-    def initialize(*args)
-      super
-      self.codepoint = self.codepoint.to_i(16) if self.codepoint && self.codepoint !~ /\.\./
-                                                  # TODO cleaner    # FIXME ranges
-    end
-
-    def self.from_line(line)
-      line =~ /(.*);(.*) # (.*)$/
-        raise 'BUG' unless line
-      new $1,$2,$3
+  def table
+    if @table
+      @table
+    else
+      build_table unless File.file?(TABLE_FILE)
+      @table = Marshal.load File.respond_to?(:binread) ? File.binread(TABLE_FILE) : File.read(TABLE_FILE)
     end
   end
 
-  def self.line(n)
+  def codepoint(n)
+    n = n.to_s.unpack('U')[0] unless n.is_a? Integer
+    table[n] or raise ArgumentError
+  end
+  alias width codepoint
+  alias of    codepoint
+
+  def build_table
     data.rewind
-    offset = offsets[n] or raise ArgumentError
-    data.seek offset
-    data.readline.chomp
-  end
-
-  def self.codepoint(n)
-    Codepoint.from_line line(n)
-  end
-
-  def self.valid_index?
-    !!offsets rescue false
-  end
-
-  def self.build_index
-    data.rewind
-    offsets = {}
-    dir = File.dirname INDEX_FILE
+    table = {}
+    dir = File.dirname TABLE_FILE
     Dir.mkdir(dir) unless Dir.exists?(dir)
-    data.lines.map do |line|
-      offsets[Codepoint.from_line(line).codepoint] = data.pos - line.size
-    end
-    File.open(INDEX_FILE, 'wb') { |f| Marshal.dump(offsets, f) }
+    data.lines.each{ |line|
+      line =~ /^(.*);(.*) # .*$/
+      if $1 && $2
+        cps, width = $1, $2
+        if cps['..']
+          range = Range.new *cps.split('..').map{ |cp| cp.to_i(16) }
+          range.each{ |cp| table[ cp ] = width.to_sym }
+        else
+          table[ cps.to_i(16) ] = width.to_sym
+        end
+      end
+
+    }
+    File.open(TABLE_FILE, 'wb') { |f| Marshal.dump(table, f) }
   end
 end
 
 class String
-  def display_width(ambiguous=1)
+  def display_width(ambiguous = 1)
     #codepoints.inject(0){ |a,c|
     unpack('U*').inject(0){ |a,c|
-      width = case Unicode::DisplayWidth.codepoint(c).width
+      width = case Unicode::DisplayWidth.codepoint(c).to_s
               when *%w[F W]
                 2
               when *%w[N Na H]
