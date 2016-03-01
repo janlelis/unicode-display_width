39e4de818043c6cd066c5cbfff461d8ea0a09510
Jan Lelis <mail@janlelis.de>
2016-03-01 19:29:03 +0100

    Refactor internals, different index format

--- lib/unicode/display_width.rb
+++ lib/unicode/display_width.rb
@@ -1,60 +1,40 @@
 module Unicode
   module DisplayWidth
     VERSION = '0.3.1'.freeze
-    DATA_DIR = File.join(File.dirname(__FILE__), '../../data/').freeze
-    EAST_ASIAN_WIDTH_INDEX_FILENAME = (DATA_DIR + 'EastAsianWidth.index').freeze
-    EAST_ASIAN_WIDTH_DATA_FILENAME  = (DATA_DIR + 'EastAsianWidth.txt').freeze
-    GENERAL_CATEGORY_WIDTH_INDEX_FILENAME = (DATA_DIR + 'GeneralCategoryWidth.index').freeze
+    DATA_DIRECTORY = File.join(File.dirname(__FILE__), '../../data/').freeze
+    INDEX_FILENAME = (DATA_DIRECTORY + 'EastAsianWidth.index').freeze
 
     class << self
-      # A complete mapping from codepoint to (east asian) width
-      def east_asian_width_table
-        if defined?(@east_asian_width_table) && @east_asian_width_table
-          @east_asian_width_table
+      def index
+        if defined?(@index) && @index
+          @index
         else
-          @east_asian_width_table = Marshal.load(File.binread(EAST_ASIAN_WIDTH_INDEX_FILENAME))
+          @index = Marshal.load(File.binread(INDEX_FILENAME))
         end
-      end
-
-      def east_asian_width(n)
-        n = n.to_s.unpack('U')[0] unless n.is_a? Integer
-        east_asian_width_table[n] or raise ArgumentError, 'codepoint not found'
-      end
-
-      # A table that defines values for some special characters (typically zero width)
-      def general_category_width_table
-        if defined?(@general_category_width_table) && @general_category_width_table
-          @general_category_width_table
-        else
-          @general_category_width_table = Marshal.load(File.binread(GENERAL_CATEGORY_WIDTH_INDEX_FILENAME))
-        end
-      end
-
-      def general_category_width(n)
-        n = n.to_s.unpack('U')[0] unless n.is_a? Integer
-        general_category_width_table[n]
       end
 
       def for(string, ambiguous = 1)
         string.unpack('U*').inject(0){ |total_width, char|
-          total_width + ( general_category_width(char) ||
-            case east_asian_width(char)
-            when :F, :W
-              2
-            when :N, :Na, :H
-              1
-            when :A
-              ambiguous
-            else
-              1
-            end
-          )
+          total_width + case width = index[char]
+          when Integer
+            width
+          when :F, :W
+            2
+          when :N, :Na, :H
+            1
+          when :A
+            ambiguous
+          else
+            1
+          end
         }
       end
     end
   end
 end
 
+# Allows you to opt-out of the default string extension. Will eventually be removed,
+# so you must opt-in for the core extension by requiring 'display_width/string_ext'
 unless defined?(Unicode::DisplayWidth::NO_STRING_EXT) && Unicode::DisplayWidth::NO_STRING_EXT
   require_relative 'display_width/string_ext'
 end
--- lib/unicode/display_width/index_builder.rb
+++ lib/unicode/display_width/index_builder.rb
@@ -3,6 +3,10 @@ require_relative '../display_width'
 module Unicode
   module DisplayWidth
     module IndexBuilder
+      EAST_ASIAN_WIDTH_DATA_FILENAME = (DATA_DIRECTORY + 'EastAsianWidth.txt').freeze
+      ZERO_WIDTH_CATEGORIES = %w[Mn Me]
+      SPECIAL_WIDTHS = {}
+
       def self.fetch!
         require 'open-uri'
         open("http://www.unicode.org/Public/UNIDATA/EastAsianWidth.txt") { |f|
@@ -11,12 +15,10 @@ module Unicode
       end
 
       def self.build!
-        data = File.open EAST_ASIAN_WIDTH_DATA_FILENAME
+        data = File.open(EAST_ASIAN_WIDTH_DATA_FILENAME)
         data.rewind
-        Dir.mkdir(DATA_DIR) unless Dir.exists?(DATA_DIR)
-        east_asian_width_table = {}
-        general_category_width_table = {}
-        zero_width_category = "Mn"
+        Dir.mkdir(DATA_DIRECTORY) unless Dir.exists?(DATA_DIRECTORY)
+        index = {}
 
         data.each_line{ |line|
           line =~ /^(\S+?);(\S+)\s+#\s(\S+).*$/
@@ -24,17 +26,19 @@ module Unicode
             cps, width, category = $1, $2, $3
             if cps['..']
               Range.new(*cps.split('..').map{ |cp| cp.to_i(16) }).each{ |cp|
-                east_asian_width_table[cp] = width.to_sym
-                general_category_width_table[cp] = 0 if category == zero_width_category
+                index[cp] = is_zero_width?(category) ? 0 : width.to_sym
               }
             else
-              east_asian_width_table[cps.to_i(16)] = width.to_sym
-              general_category_width_table[cps.to_i(16)] = 0 if category == zero_width_category
+              index[cps.to_i(16)] = is_zero_width?(category) ? 0 : width.to_sym
             end
           end
         }
-        File.open(EAST_ASIAN_WIDTH_INDEX_FILENAME, 'wb') { |f| Marshal.dump(east_asian_width_table, f) }
-        File.open(GENERAL_CATEGORY_WIDTH_INDEX_FILENAME, 'wb') { |f| Marshal.dump(general_category_width_table, f) }
+        index.merge! SPECIAL_WIDTHS
+        File.open(INDEX_FILENAME, 'wb') { |f| Marshal.dump(index, f) }
+      end
+
+      def self.is_zero_width?(category)
+        ZERO_WIDTH_CATEGORIES.include?(category)
       end
     end
   end
