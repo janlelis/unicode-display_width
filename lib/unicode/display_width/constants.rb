006f718ab21c5fee62c5a0aaf47db39b820ffb38
Jan Lelis <mail@janlelis.de>
2016-03-02 18:57:13 +0100

    Release 1.0

--- lib/unicode/display_width/constants.rb
+++ lib/unicode/display_width/constants.rb
@@ -1,6 +1,6 @@
 module Unicode
   module DisplayWidth
-    VERSION = '0.3.1'.freeze
+    VERSION = '1.0.0'.freeze
     DATA_DIRECTORY = File.join(File.dirname(__FILE__), '../../../data/').freeze
     INDEX_FILENAME = (DATA_DIRECTORY + 'unicode-width.index').freeze
   end
