94975221a7487633e0aed51a2f63c98aa975f0fb
Jan Lelis <mail@janlelis.de>
2016-04-30 11:36:55 +0200

    Release 1.0.4

--- lib/unicode/display_width/constants.rb
+++ lib/unicode/display_width/constants.rb
@@ -1,6 +1,6 @@
 module Unicode
   module DisplayWidth
-    VERSION = '1.0.3'
+    VERSION = '1.0.4'
     UNICODE_VERSION = "8.0.0".freeze
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + '/../../../data/').freeze
     INDEX_FILENAME = (DATA_DIRECTORY + '/display_width.marshal.gz').freeze
