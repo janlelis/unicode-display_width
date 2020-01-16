5ac82b017742dcf75fc9406dacb3e358cc4e4292
Jan Lelis <hi@ruby.consulting>
2020-01-16 13:48:16 +0100

    Release v1.6.1

--- lib/unicode/display_width/constants.rb
+++ lib/unicode/display_width/constants.rb
@@ -2,7 +2,7 @@
 
 module Unicode
   class DisplayWidth
-    VERSION = "1.6.0"
+    VERSION = "1.6.1"
     UNICODE_VERSION = "12.1.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/")
     INDEX_FILENAME = DATA_DIRECTORY + "/display_width.marshal.gz"
