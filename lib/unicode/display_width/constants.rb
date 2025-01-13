a515fa2c6898a2a886702c012cad6541fa2386e1
Jan Lelis <hi@syntaxtransformation.com>
2025-01-13 13:07:31 +0100

    Release v3.1.4

--- lib/unicode/display_width/constants.rb
+++ lib/unicode/display_width/constants.rb
@@ -2,7 +2,7 @@
 
 module Unicode
   class DisplayWidth
-    VERSION = "3.1.3"
+    VERSION = "3.1.4"
     UNICODE_VERSION = "16.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/")
     INDEX_FILENAME = DATA_DIRECTORY + "/display_width.marshal.gz"
